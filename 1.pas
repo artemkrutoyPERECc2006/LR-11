c9742d7564e0e9d7037274a1db85d134f9330298
Стив Цукерберг <144227435+artemkrutoyPERECc2006@users.noreply.github.com>
2023-12-15 12:35:25 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,19 @@
+﻿Program zad1;
+uses GraphABC;
+begin
+  SetWindowSize(1000,700);
+  SetPenColor(clBlack);
+  line(400,350,700,350);
+  moveTo(700,350);
+  lineto(550,250);
+  lineto(400,350);
+  FloodFill(500,300,clblue);
+  moveto(700,350);
+  lineto(550,450);
+  lineto(400,350);
+  FloodFill(500,400,clgreen);
+  Circle(350,350,50);
+  FloodFill(350,350,clred);
+  Circle(750,350,50);
+  FloodFill(750,350,clyellow)
+end.
